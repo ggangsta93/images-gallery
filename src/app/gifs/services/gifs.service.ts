5b600da3e68d2d2fb89cfbe4b342fe8f05e67f8b
Javier Arias <parias@heinsohn.com.co>
2022-10-01 13:46:34 -0500

    Se agrega llamado de API

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,21 +6,34 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apiKey: string = 'AvU6SBvL1MBI6SrA0JTyszQiRWzZKHKl';
   private _historial: string[] = [];
+  public resultados: any[]=[]
+
+  constructor(private http: HttpClient) { }
 
   get historial() {
     return [...this._historial];
   }
-  
-  buscarGifs(valor: string='') {
+
+  async buscarGifs(valor: string = '') {
     valor = valor.trim().toLocaleLowerCase();
-    
-    if(!this._historial.includes(valor)){
+
+    if (!this._historial.includes(valor)) {
       this._historial.unshift(valor);
-      this._historial= this._historial.splice(0,10);
+      this._historial = this._historial.splice(0, 10);
     }
-    console.log(this._historial);
+    //console.log(this._historial);
+
+    /*respo es de tipo any porque typescript no tiene manera de saber 
+    que respuesta va regresar ya no es quien realiza la petición http */
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=AvU6SBvL1MBI6SrA0JTyszQiRWzZKHKl&q=${valor}&limit=10`)
+      .subscribe((respo: any) => {
+        console.log(respo.data);
+        this.resultados=respo.data;
+      })
+
+
   }
 
-  constructor() { }
 }
