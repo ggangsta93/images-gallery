fdf1fb02489c41941cf89a823e214ac3d58a9dbc
Javier Arias <parias@heinsohn.com.co>
2022-10-02 10:22:04 -0500

    Se ajusta url con HttpParams

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SearchImagesResponse, Image } from '../interface/images.interface';
 
@@ -8,12 +8,13 @@ import { SearchImagesResponse, Image } from '../interface/images.interface';
 export class GifsService {
 
   private apiKey: string = 'AvU6SBvL1MBI6SrA0JTyszQiRWzZKHKl';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
-  public resultados: Image[]=[];
+  public resultados: Image[] = [];
 
   constructor(private http: HttpClient) {
-    this._historial= JSON.parse(localStorage.getItem('historial')!) || [];
-    this.resultados= JSON.parse(localStorage.getItem('resultados')!) || [];
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
 
   }
 
@@ -27,19 +28,23 @@ export class GifsService {
     if (!this._historial.includes(valor)) {
       this._historial.unshift(valor);
       this._historial = this._historial.splice(0, 10);
-      localStorage.setItem('historial',JSON.stringify(this._historial));
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
-    //console.log(this._historial);
+
+    /*
+      HttpParams
+    */
+    const params = new HttpParams().set('api_key', this.apiKey).set('limit', '10').set('q', valor);
+    console.log(params.toString());
 
     /*respo es de tipo any porque typescript no tiene manera de saber 
     que respuesta va regresar ya que no es quien realiza la petición http; 
     para solucionar el tipo de dato se crea una interface */
-
-    this.http.get<SearchImagesResponse>(`https://api.giphy.com/v1/gifs/search?api_key=AvU6SBvL1MBI6SrA0JTyszQiRWzZKHKl&q=${valor}&limit=10`)
+    this.http.get<SearchImagesResponse>(`${this.servicioUrl}/search`,{params:params})
       .subscribe((respo) => {
         console.log(respo.data);
-        this.resultados=respo.data;
-        localStorage.setItem('resultados',JSON.stringify(this.resultados));
+        this.resultados = respo.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
 
       })
 
